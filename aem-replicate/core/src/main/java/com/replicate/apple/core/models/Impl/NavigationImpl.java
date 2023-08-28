b3a49fc22703983c3f148dc62380baa7ade1202b
Jyotiswarup <jyotijohn305@gmail.com>
2023-08-29 01:17:21 +0530

    ResourceUtil file

--- aem-replicate/core/src/main/java/com/replicate/apple/core/models/Impl/NavigationImpl.java
+++ aem-replicate/core/src/main/java/com/replicate/apple/core/models/Impl/NavigationImpl.java
@@ -3,7 +3,9 @@ package com.replicate.apple.core.models.Impl;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,15 +20,26 @@ public class NavigationImpl implements Navigation {
 
   public final static Logger log = LoggerFactory.getLogger(NavigationImpl.class);
 
+  @OSGiService
+  ResourceResolverFactory resourceResolverFactory;
+
+  String title;
+
   @Override
   public String getPath() {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPath'");
+
+    try {
+      ResourceResolver resolver = Resolverutil.newResolver(resourceResolverFactory);
+      PageManager pageManager = resolver.adaptTo(PageManager.class);
+      Page page = pageManager.getPage("/content/apple-replicate/language-masters/en/apple-replicate-home-page");
+      this.title = page.getTitle().toString();
+    } catch (LoginException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return "value" + title;
   }
 
-
- }
-
-
-
-
+}
--- aem-replicate/core/src/main/java/com/replicate/apple/core/utils/Resolverutil.java
+++ aem-replicate/core/src/main/java/com/replicate/apple/core/utils/Resolverutil.java
@@ -1,31 +1,39 @@
 package com.replicate.apple.core.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.osgi.service.component.annotations.Reference;
 
+import java.util.HashMap;
+import java.util.Map;
+
+// resource resolver factory helper class
+
 public final class Resolverutil {
 
-    public static final String apple_replicate="applereplicateadmin";
-
-    @Reference
-    private static ResourceResolverFactory resolverFactory;
-
-
-    public static ResourceResolver newResolver() throws LoginException{
-
-       final Map<String,Object> map=new HashMap<String , Object>();
-
-       map.put(ResourceResolverFactory.SUBSERVICE, apple_replicate);
-       
-       ResourceResolver resolver=resolverFactory.getResourceResolver(map);
-
-
-        return resolver;
-    }
+  private Resolverutil() {
     
+
+
+  }
+
+  public static final String apple_replicate = "applereplicateadmin";
+
+  /**
+   * @param resourceResolverFactory factory
+   * @return new resource resolver for Sony service user
+   * @throws LoginException if problems
+   */
+  public static  ResourceResolver newResolver(ResourceResolverFactory resourceResolverFactory) throws LoginException {
+    final Map<String, Object> paramMap = new HashMap<String, Object>();
+    // created a map object
+
+    paramMap.put(ResourceResolverFactory.SUBSERVICE, apple_replicate);
+
+    // fetches the admin service resolver using service user.
+    ResourceResolver resolver = resourceResolverFactory.getServiceResourceResolver(paramMap);
+    return resolver;
+  }
+
 }
